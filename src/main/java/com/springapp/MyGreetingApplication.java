e33b8d2c954cd8ac9ebb124912b8f91460245255
sunilmeena26 <sunilmeena641085@gmail.com>
2025-02-24 02:32:49 +0530

    [Sunil Meena] Add My Greeting Application main class code file

---
+++ src/main/java/com/springapp/MyGreetingApplication.java
@@ -0,0 +1,13 @@
+package com.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGreetingApplication.class, args);
+	}
+
+}
