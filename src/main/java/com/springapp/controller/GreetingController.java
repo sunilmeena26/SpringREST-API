a2fb339a0c395747bbe98adccb6cfca7b9a181f1
sunilmeena26 <sunilmeena641085@gmail.com>
2025-02-24 11:29:47 +0530

    [Sunil Meena] Add GreetingController to find the greeting message by id in the repository code file

---
+++ src/main/java/com/springapp/controller/GreetingController.java
@@ -0,0 +1,49 @@
+package com.springapp.controller;
+
+import com.springapp.entity.Greeting;
+import com.springapp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+//Create a Controller class to handle HTTP requests related to greeting
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    //Create an instance of GreetingService It is automatically injects
+    @Autowired
+    private GreetingService greetingService;
+
+
+    //Create a method to handle get request
+    @GetMapping(value = {"/hello"})
+    public String getGreeting(){
+        return "{\n\t\"message\" : \"Hello World!\"\n}";
+    }
+
+    //Create a method getSimpleGreeting to return the simple greeting message
+    @GetMapping("/simplegreeting")
+    public String getSimpleGreeting(){
+        return greetingService.getSimpleGreeting();
+    }
+
+    //Create a getGreetingWithUserDetails method to print greeting massage with person details like their first and last name
+    @GetMapping("/personalized")
+    public String getGreetingWithUserDetails(@RequestParam(required = false)String firstName, @RequestParam(required = false)String lastName){
+        return greetingService.getUserGreeting(firstName,lastName);
+    }
+
+    //Create a method saveGreeting to saves a Greeting entity to the database
+    @PostMapping("/save")
+    public Greeting saveGreeting(@RequestBody Greeting greeting){
+        return greetingService.saveGreeting(greeting);
+    }
+
+    //Create a method getGreetingById to search greeting massage using id number
+    @GetMapping("/{id}")
+    public Optional<Greeting> getGreetingById(@PathVariable Long id){
+        return  greetingService.findGreetingById(id);
+    }
+}
